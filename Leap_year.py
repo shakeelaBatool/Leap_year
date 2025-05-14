39b31d4a325096664e42ae10b37ae4c14a5fc9a3
shakeelaBatool <shakeelasadpara@gmail.com>
2025-05-14 12:48:14 -0700

    Add files via upload

---
+++ Leap_year.py
@@ -0,0 +1,13 @@
+
+
+def leap_year(year):
+
+    if (year % 400 == 0) and (year % 100 == 0):
+        print(year," is a Leap year")
+    elif (year % 4 == 0) and (year % 100 != 0):
+            print(year," is a leap year. ")
+    else:
+            print(year, "is not leap year.") 
+    return year
+year=int(input("Enter the year. "))  
+(leap_year(year))  
