6a572f0b380b4fcd589533414388e1fb40f52c69
Shakeela Batool <shakeelasadpara@gmail.com>
2025-11-17 21:20:50 +0500

    Update Leap_year.py

--- Leap_year.py
+++ Leap_year.py
@@ -4,13 +4,14 @@ def leap_year(year):
 
     if (year % 400 == 0) and (year % 100 == 0):
         print(year," is a Leap year")
-    elif (year % 4 == 0) and (year % 100 !=0):
-            print(year," is a leap year. ")
+    elif (year % 4 == 0) and (year % 100 != 0):
+            print(year ," is a leap year. ")
     else:
             print(year, "is not a leap year.") 
     return year
 year=int(input("Enter the year. "))  
-(leap_year(year))  
+pritn(leap_year(year))  
+
 
 
 
