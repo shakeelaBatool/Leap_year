c5c3112810a8ecffce10fd00a1eddc30fb250439
Shakeela Batool <shakeelasadpara@gmail.com>
2025-12-07 20:38:51 +0500

    Fix indentation and formatting in leap_year function

--- Leap_year.py
+++ Leap_year.py
@@ -4,13 +4,14 @@ def leap_year( year):
 
     if (year % 400 == 0) and (year % 100 == 0):
         print(year," is a Leap year")
-    elif (year % 4 == 0) and (year % 100 != 0):
+    elif (year % 4 ==0) and (year % 100 != 0):
             print(year ," is a leap year. ")
     else:
             print(year, "is not a leap year.") 
     return year
 year=int(input("Enter the year. "))  
 pritn(leap_year( year ))  
+
 
 
 
